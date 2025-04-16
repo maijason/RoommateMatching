932cd91d3aba0474cc4f83b93fd082467fb6d84e
jasonlam11 <166332320+jasonlam11@users.noreply.github.com>
2025-04-16 18:07:37 -0400

    updated rest_entry with system admin

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -6,6 +6,7 @@ from backend.products.products_routes import products
 from backend.simple.simple_routes import simple_routes
 from backend.students.student_routes import students
 from backend.dorms.dorm_routes import dorms
+from backend.system_admin.system_admin_routes import system_admin
 import os
 from dotenv import load_dotenv
 
@@ -46,6 +47,7 @@ def create_app():
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(students,    url_prefix='/s')
     app.register_blueprint(dorms,    url_prefix='/d')
+    app.register_blueprint(system_admin,    url_prefix='/admin')
 
     # Don't forget to return the app object
     return app
