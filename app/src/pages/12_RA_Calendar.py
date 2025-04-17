ca96243aead6ac8a68d2fb93aa72cd769181e514
[emilyquackkk] <quach.e@northeastern.edu>
2025-04-16 21:46:03 -0400

    calander fix with date

--- app/src/pages/12_RA_Calendar.py
+++ app/src/pages/12_RA_Calendar.py
@@ -13,6 +13,30 @@ SideBarLinks()
 
 st.title("📅 RA Calendar")
 
+with st.form("new_event"):
+    st.write("➕ Create New Event")
+    title = st.text_input("Title")
+    location = st.text_input("Location")
+    date = st.date_input("Date")
+    time = st.time_input("Time")
+    submit = st.form_submit_button("Add")
+
+    if submit:
+        from datetime import datetime
+        full_datetime = datetime.combine(date, time)
+
+        try:
+            res = requests.post("http://localhost:4000/events/create", json={
+                "title": title,
+                "location": location,
+                "datetime": str(full_datetime)
+            })
+            if res.status_code == 201:
+                st.success("Event added.")
+        except:
+            st.error("Could not add event.")
+
+
 # Get event data
 try:
     response = requests.get("http://localhost:4000/events/upcoming")
